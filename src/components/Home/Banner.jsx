1a2aa30c0ed5c6064926e6a492ca076e70df60e0
ruhitbaidya <ruhitbaidya01@gmail.com>
2025-01-18 21:22:52 +0600

    this slider work sinish

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -7,8 +7,8 @@ import img1 from "../../assets/finialbgs.png";
 import offer from "../../assets/Offer.png";
 import light from "../../assets/image.png";
 import Buttons from "../ReuserComponents/Buttons";
-// import charger from "../../assets/cnarger.png";
-// import images2 from "../../assets/hero2.png";
+import charger from "../../assets/cnarger.png";
+import images2 from "../../assets/hero2.png";
 
 export const Banner = () => {
   return (
@@ -27,12 +27,12 @@ export const Banner = () => {
           <div className="">
             <div className="relative">
               <img className="w-full" src={img1} alt="" />
-              <div className="lg:flex justify-between items-center top-0 h-full px-[40px] absolute">
-                <div className="lg:w-[60%] flex-col justify-center">
-                  <h1 className="lg:text-[64px] text-[25px] text-white">
+              <div className="flex justify-between items-center top-0 h-full px-[40px] absolute">
+                <div className="w-[60%]">
+                  <h1 className="lg:text-[64px] text-[18px] text-white">
                     Bandon Electronics and Lighting Store
                   </h1>
-                  <p className="text-[15px] lg:mt-[20px] lg:w-[60%] text-white">
+                  <p className="text-[15px] hidden lg:flex lg:mt-[20px] lg:w-[60%] text-white">
                     BEALS is tailored to the needs of your seeking high-quality
                     electronics and lighting solutions.
                   </p>
@@ -40,13 +40,13 @@ export const Banner = () => {
                     <Buttons text="Bye Now" />
                   </div>
                 </div>
-                <div className="hidden lg:flex w-[40%]">
-                  <div className="w-full flex justify-center items-center">
+                <div className="w-[40%]">
+                  <div className="w-full flex-col justify-center items-center">
                     <div className="relative">
-                      <img className="" src={light} alt="light" />
-                      <div className="absolute bottom-0 -left-12">
+                      <img className="object-contain" src={light} alt="light" />
+                      <div className="absolute bottom-0 lg:-left-12">
                         <img
-                          className="image-offer image-full hover:rotate-12 transition-[0.06s] hover:shadow-lime-300"
+                          className="image-offer lg:w-auto w-[80px] hover:rotate-12 transition-[0.06s]"
                           src={offer}
                           alt="offer"
                         />
@@ -58,16 +58,16 @@ export const Banner = () => {
             </div>
           </div>
         </SwiperSlide>
-        {/* <SwiperSlide>
+        <SwiperSlide>
           <div className="">
             <div className="relative">
               <img className="w-full" src={images2} alt="" />
-              <div className="flex justify-between items-center top-0 h-full px-[40px] w-full absolute">
+              <div className="flex justify-between items-center top-0 h-full px-[40px] absolute">
                 <div className="w-[60%]">
-                  <h1 className="text-[64px] text-black">
+                  <h1 className="lg:text-[64px] text-[18px] text-black">
                     We Promise <br /> Your Choice
                   </h1>
-                  <p className="mt-[20px] w-[60%] text-black">
+                  <p className="text-[15px] hidden lg:flex lg:mt-[20px] lg:w-[60%] text-black">
                     BEALS is tailored to the needs of your seeking high-quality
                     electronics and lighting solutions.
                   </p>
@@ -76,12 +76,16 @@ export const Banner = () => {
                   </div>
                 </div>
                 <div className="w-[40%]">
-                  <div className="w-full flex justify-center items-center">
+                  <div className="w-full flex-col justify-center items-center">
                     <div className="relative">
-                      <img className="" src={charger} alt="charger" />
-                      <div className="absolute bottom-0 -left-12">
+                      <img
+                        className="object-contain"
+                        src={charger}
+                        alt="light"
+                      />
+                      <div className="absolute bottom-0 lg:-left-12">
                         <img
-                          className="image-offer image-full hover:rotate-12 transition-[0.06s] hover:shadow-lime-300"
+                          className="image-offer lg:w-auto w-[80px] hover:rotate-12 transition-[0.06s]"
                           src={offer}
                           alt="offer"
                         />
@@ -92,7 +96,7 @@ export const Banner = () => {
               </div>
             </div>
           </div>
-        </SwiperSlide> */}
+        </SwiperSlide>
       </Swiper>
     </div>
   );
